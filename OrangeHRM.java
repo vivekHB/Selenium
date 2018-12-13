af2485a0d13c54e583db6a72e0752512a8eadfe7
vivekHB <45853538+vivekHB@users.noreply.github.com>
2018-12-13 21:10:09 +0530

    Update OrangeHRM.java

--- OrangeHRM.java
+++ OrangeHRM.java
@@ -8,19 +8,14 @@ public class OrangeHRM {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		//Open Fire fox Browser
 		WebDriver driver=new FirefoxDriver();
 		
-		//Open URL In Browser
 		driver.navigate().to("http://opensource.demo.orangehrmlive.com/");
 		
-		//Get the Title of WebPage
 		String title=driver.getTitle();
 		
-		//print the title of the web page
 		System.out.println(title);
 		
-		//Verify Title of WebPage
 		if(title.equals("OrangeHRM"))
 		{
 			System.out.println("Title Verified!!!!");
@@ -29,13 +24,10 @@ public class OrangeHRM {
 			System.out.println("Title is Different from expected");
 		}
 		
-		//Enter the user name
 		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
 		
-		//Enter the password
 		driver.findElement(By.id("txtPassword")).sendKeys("admin");
 		
-		//Clicking On Login Button
 		driver.findElement(By.id("btnLogin")).click();
 		
 		
@@ -43,13 +35,10 @@ public class OrangeHRM {
 
 		Thread.sleep(5000);
 		
-		//Identify Welcome selenium
 		String welcome=driver.findElement(By.id("welcome")).getText();
 		
-		//Print welcome message
 		System.out.println(welcome);
 		
-		//To verify whether the welcome page successfully opened or not
 		if(welcome.equals("Welcome Admin"))
 		{
 			System.out.println("Welcome page verified!!");
@@ -57,7 +46,6 @@ public class OrangeHRM {
 			System.out.println("Welcome message is different from expected");
 		}
 		
-		//click logout
 		driver.findElement(By.id("welcome")).click();
 	
 			Thread.sleep(5000);
